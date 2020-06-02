61c4087b19c9b958e34e446d840953ccecdbd3c1
JosWambugu <josphatwambugu77@gmail.com>
2020-06-02 16:22:22 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,9 @@
+using System;
+public class HelloWorld
+{
+    public static void main(String[]args){
+ Console.WriteLine("Hello world");
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+public class ExampleForm : Form // inherits from System.Windows.Forms.Form
+{
+public static void Main()
+{
+ExampleForm wikibooksForm = new ExampleForm();
+wikibooksForm.Text = "I Love Wikibooks";// specify title of the form
+wikibooksForm.Width = 400; // width of the window in pixels
+wikibooksForm.Height = 300; // height in pixels
+Application.Run(wikibooksForm); // display the form
+}
+}
